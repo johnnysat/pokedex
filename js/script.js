d643e9386da026d3640a7ce9a48c8b897266cc28
Johnny <johnny_s.o@hotmail.com>
2022-08-12 19:29:00 -0300

    arrumando pokemonsearch

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const inputPokemon = document.querySelector('.input__search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
-let searchPokemon = 501;
+let searchPokemon = 1;
 
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
